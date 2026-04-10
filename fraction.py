8f127a3dbfc81d0e6efa27163fab37ca5909ddf4
LAVstyle <luchin250398@yandex.ru>
2026-04-10 21:20:05 +0700

    logistic: update of initial values

--- fraction.py
+++ fraction.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 # данные
-x = np.array([0.0, 0.1, 0.2, 0.3, 0.4])
-y = np.array([0.02, 0.10, 0.35, 0.65, 0.85])
+x = np.array([0.0, 0.05, 0.1, 0.2, 0.4, 0.6])
+y = np.array([0.0, 0.1, 0.15, 0.35, 0.50, 0.60])
 
 # логистическая функция
 def logistic(x, k, x0):
@@ -17,7 +17,7 @@ k, x0 = params
 print(f"k = {k:.3f}, x0 = {x0:.3f}")
 
 # гладкий график
-x_fit = np.linspace(0, 0.5, 100)
+x_fit = np.linspace(0, 0.6, 100)
 y_fit = logistic(x_fit, k, x0)
 
 # график
